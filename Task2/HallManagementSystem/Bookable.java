7200c4bc450c6b94324e597e1c23cc095668889c
Mahazabin <mahazabinmunia@gmail.com>
2024-12-01 12:19:59 +0600

    Delete Task2/HallManagementSystem directory

--- Task2/HallManagementSystem/Bookable.java
+++
@@ -1,9 +0,0 @@
-
-package HallManagementSystem;
-
-// Bookable interface: ISP by defining methods for availability management
-
-public interface Bookable {
-    boolean isAvailable(); // Checks if the room is available for booking
-    void setAvailability(boolean availability); // Sets the availability status of the room
-}
--- Task2/HallManagementSystem/Payable.java
+++
@@ -1,9 +0,0 @@
-
-package HallManagementSystem;
-
-// Payable interface: ISP by having only specific payment-related methods
-
-public interface Payable {
-    void payFee(double amount);
-    boolean isPaid();
-}
